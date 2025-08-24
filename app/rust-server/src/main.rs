849355af4fb4f93d2e972cf2c1657038ad767e40
zhoutongsheng <zhoutongsheng@haizhi.com>
2025-08-24 14:11:24 +0800

    update

--- app/rust-server/src/main.rs
+++ app/rust-server/src/main.rs
@@ -2,19 +2,42 @@ mod proxy;
 mod guard_sse_stream;
 mod handler;
 mod common;
+mod mcp;
 
 use axum::{
     routing::{get, post}, Router,
 };
-use tracing::info;
+use rmcp::transport::sse_server::SseServerConfig;
+use rmcp::transport::SseServer;
+use tracing::{error, info};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use crate::mcp::ChromeExtensionServer;
 use crate::proxy::ProxyState;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Initialize tracing
-    tracing_subscriber::fmt()
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "debug".to_string().into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
         .init();
+
+    let addr = "0.0.0.0:12306";
+
+    let config = SseServerConfig {
+        bind: addr.parse()?,
+        sse_path: "/sse".to_string(),
+        post_path: "/message".to_string(),
+        ct: tokio_util::sync::CancellationToken::new(),
+        sse_keep_alive: None,
+    };
+
+    let (sse_server, router) = SseServer::new(config);
 
     let proxy_state = ProxyState::new();
 
@@ -25,11 +48,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/api/tool/call", get(handler::call_tool))
         .with_state(proxy_state);
 
-    let addr = "0.0.0.0:12306";
-    info!("Starting server on {}", addr);
+    let listener = tokio::net::TcpListener::bind(sse_server.config.bind).await?;
 
-    let listener = tokio::net::TcpListener::bind(&addr).await?;
-    axum::serve(listener, app).await?;
+    let ct = sse_server.with_service(ChromeExtensionServer::default);
+
+    // Handle signals for graceful shutdown
+    let cancel_token = ct.clone();
+    tokio::spawn(async move {
+        match tokio::signal::ctrl_c().await {
+            Ok(()) => {
+                println!("Received Ctrl+C, shutting down server...");
+                cancel_token.cancel();
+            }
+            Err(err) => {
+                eprintln!("Unable to listen for Ctrl+C signal: {}", err);
+            }
+        }
+    });
+
+    // Start HTTP server
+    info!("Server started on {}", addr);
+    let server = axum::serve(listener, app).with_graceful_shutdown(async move {
+        // Wait for cancellation signal
+        ct.cancelled().await;
+        info!("Server is shutting down...");
+    });
+
+    if let Err(e) = server.await {
+        error!("Server error: {}", e);
+    }
+
+    info!("Server has been shut down");
 
     Ok(())
 }
---
+++ app/rust-server/src/mcp.rs
@@ -0,0 +1,50 @@
+use rmcp::handler::server::tool::ToolCallContext;
+use rmcp::model::{CallToolRequestParam, CallToolResult, ErrorCode, Implementation, JsonObject, ListToolsResult, PaginatedRequestParam, ProtocolVersion, ServerCapabilities, ServerInfo, Tool};
+use rmcp::{ErrorData, RoleServer, ServerHandler};
+use rmcp::serde_json::from_str;
+use rmcp::service::RequestContext;
+
+#[derive(Clone, Default)]
+pub struct ChromeExtensionServer;
+
+impl ServerHandler for ChromeExtensionServer {
+    async fn call_tool(
+        &self,
+        request: CallToolRequestParam,
+        context: RequestContext<RoleServer>,
+    ) -> Result<CallToolResult, ErrorData> {
+        let tcc = ToolCallContext::new(self, request, context);
+        // self.tool_router.call(tcc).await
+        println!("call tool: {}, {:?}", tcc.name, tcc.arguments);
+        Err(ErrorData::new(ErrorCode(1), "test".to_string(), None))
+    }
+
+    async fn list_tools(
+        &self,
+        _request: Option<PaginatedRequestParam>,
+        _context: RequestContext<RoleServer>,
+    ) -> Result<ListToolsResult, ErrorData> {
+        tracing::info!("Listing tools");
+        let input_schema = from_str::<JsonObject>(r#"
+        {
+            "type": "object",
+            "properties": {},
+            "required": []
+        }
+        "#).expect("Failed to parse input schema");
+        let tool = Tool::new("test", "Test tool", input_schema);
+        // let items = self.tool_router.list_all();
+        Ok(ListToolsResult::with_all_items(vec![tool]))
+    }
+
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo {
+            protocol_version: ProtocolVersion::V_2024_11_05,
+            capabilities: ServerCapabilities::builder()
+                .enable_tools()
+                .build(),
+            server_info: Implementation::from_build_env(),
+            instructions: Some("Chrome Extension MCP Server.".to_string()),
+        }
+    }
+}
